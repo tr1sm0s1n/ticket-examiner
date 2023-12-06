a6ed51a4843fa3aa2f01d7628092e61e393dc122
sachin.mi20 <sachin.mi20@iiitmk.ac.in>
2023-12-06 16:33:57 +0530

    Analytics screen

--- lib/home/analytics.dart
+++ lib/home/analytics.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/percent_indicator.dart';
+import 'package:http/http.dart' as http;
 
 class Analytics extends StatefulWidget {
   const Analytics({super.key});
@@ -9,75 +12,177 @@ class Analytics extends StatefulWidget {
 }
 
 class _AnalyticsState extends State<Analytics> {
+  void initState() {
+    super.initState();
+    _initializeData();
+  }
+
+  List participantList = [];
+  int totalParticipants = 0;
+  int arrivedParticipants = 0;
+  double arrivedPercentage = 0.0;
+  Future<void> _initializeData() async {
+    await _makeAPICall();
+
+    setState(() {});
+  }
+
+  Future _makeAPICall() async {
+    var token =
+        "720239d96e4bbc73d16368290547ded46571708b2e82e67ec8af1a073cf7cd11";
+
+    late var base_url = "http://143.244.129.21:2001";
+    var url = Uri.parse('$base_url/certichain/verify_json');
+    // await showAlertDialog(context);
+    var response = await http.get(url, headers: {
+      'Authorization': 'Bearer $token',
+    });
+
+    if (response.statusCode == 200) {
+      List<dynamic> participantAPICall = jsonDecode(response.body);
+      for (var i = 0; i < participantAPICall.length; i++) {
+        var participantName = participantAPICall[i]["certificate"]["candidate"]
+                ["attributes"]["name"] ??
+            "";
+        var arrived = participantAPICall[i]["issued"] ?? "";
+        participantList
+            .add({"participantName": participantName, "arrived": arrived});
+      }
+      setState(() {
+        totalParticipants = participantList.length;
+        arrivedParticipants = participantList
+            .where((participant) => participant["arrived"])
+            .length;
+        arrivedPercentage = arrivedParticipants / totalParticipants;
+      });
+      print('participantList $participantList');
+    } else {}
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0), // Set the preferred height here
-        child: AppBar(
-          title: Text('Participant List'),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0), // Set the preferred height here
+          child: AppBar(
+            backgroundColor: Colors.deepPurple,
+            title: Text('Participant List'),
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            CircularPercentIndicator(
-                radius: 60,
-                percent: 0.2,
-                progressColor: Colors.blue.shade900,
-                backgroundColor: Colors.blue.shade100,
-                animation: true,
-                animationDuration: 1000,
-                lineWidth: 12.0,
-                center: Text(
-                  (0.2 * 100).toStringAsFixed(2),
-                  style: TextStyle(color: Colors.blue.shade900, fontSize: 18),
-                )),
-            Card(
-              child: ListTile(
-                leading: Icon(
-                  Icons.person,
-                  size: 56,
-                  color: Colors.blueGrey,
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 20,
                 ),
-                title: Text(
-                    'Two-linsdsds derer erwerere werew ewrer werfer er er werre ListTile'),
-                subtitle: Text('Here is a second line'),
-              ),
-            )
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          color: Colors.deepPurple,
-          child: Container(
-            height: 80,
-            child: Row(
-              // mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: IconButton(
-                      icon: Icon(
-                        size: 30,
-                        Icons.analytics_outlined,
-                        color: Colors.white,
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        15.0), // Adjust the value as needed
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        CircularPercentIndicator(
+                          radius: 60,
+                          percent: arrivedPercentage,
+                          progressColor: Colors.deepPurple,
+                          backgroundColor: Colors.blue.shade100,
+                          animation: true,
+                          animationDuration: 1000,
+                          lineWidth: 4.0,
+                          center: Text(
+                            "${(arrivedPercentage * 100).toStringAsFixed(2)} %",
+                            style: const TextStyle(
+                                color: Colors.deepPurple, fontSize: 18),
+                          ),
+                        ),
+                        Text(
+                          "$arrivedParticipants/$totalParticipants",
+                          style: const TextStyle(fontSize: 30),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: participantList.length,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) {
+                    final participantDetail = participantList[index];
+                    var participantName = participantDetail["participantName"];
+                    bool arrived = participantDetail["arrived"];
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        shadowColor: Colors.deepPurpleAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              15.0), // Adjust the value as needed
+                        ),
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            child: Text(participantName.toString()[0]),
+                          ),
+                          title: Text(
+                            participantName.toString(),
+                          ),
+                          subtitle: Column(
+                            children: [
+                              SizedBox(
+                                height: 10,
+                              ),
+                              LinearPercentIndicator(
+                                width: 110, // Set the desired width
+                                animation: true,
+                                animationDuration: 1000,
+                                progressColor: arrived
+                                    ? Colors.green.shade600
+                                    : Colors
+                                        .red, // Change the color based on the condition
+                                backgroundColor: Colors.orange,
+                                lineHeight: 20,
+                                percent: arrived
+                                    ? 1.0
+                                    : 0.0, // Set percent based on the condition
+                                center: Text(
+                                  arrived
+                                      ? 'Arrived'
+                                      : 'Not Arrived', // Set text based on the condition
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                barRadius: const Radius.circular(15),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
-                      onPressed: () => {}),
+                    );
+                  },
+                ),
+                SizedBox(
+                  height: 40,
                 ),
               ],
             ),
-          )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Scan QR',
-        child: const Icon(Icons.camera_alt),
+          ),
+        ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Ticket Examiner',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
         textTheme: GoogleFonts.juraTextTheme(),
@@ -35,113 +36,114 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(80.0), // Set the preferred height here
-        child: AppBar(
-          title: Text(widget.title),
+    return Container(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(80.0), // Set the preferred height here
+          child: AppBar(
+            title: Text(widget.title),
+          ),
         ),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Image.asset(
-                'assets/logoBH.png',
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Image.asset(
+                  'assets/logoBH.png',
+                ),
               ),
-            ),
-            SizedBox(height: 100),
-            ElevatedButton.icon(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const HomeWidget(),
+              SizedBox(height: 100),
+              ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HomeWidget(),
+                    ),
+                  );
+                },
+                icon: SizedBox(
+                  width: 150.0, // Set the width of the SizedBox as needed
+                  height: 150.0, // Set the height of the SizedBox as needed
+                  child: Icon(
+                    Icons.qr_code_scanner,
+                    size: 150.0, // Set the size of the Icon as needed
                   ),
-                );
-              },
-              icon: SizedBox(
-                width: 150.0, // Set the width of the SizedBox as needed
-                height: 150.0, // Set the height of the SizedBox as needed
-                child: Icon(
-                  Icons.qr_code_scanner,
-                  size: 150.0, // Set the size of the Icon as needed
+                ),
+                label: Text('Scan QR Code'), // Add optional text
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(
+                        10.0), // Set border radius as needed
+                  ),
+                  elevation: 15.0, // Set the elevation as needed
                 ),
               ),
-              label: Text('Scan QR Code'), // Add optional text
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(
-                      10.0), // Set border radius as needed
-                ),
-                elevation: 15.0, // Set the elevation as needed
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-          shape: const CircularNotchedRectangle(),
-          color: Colors.deepPurple,
-          child: Container(
-            height: 80,
-            child: Row(
-              // mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: IconButton(
-                      icon: Icon(
-                        size: 30,
-                        Icons.analytics_outlined,
-                        color: Colors.white,
-                      ),
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          PageRouteBuilder(
-                            pageBuilder:
-                                (context, animation, secondaryAnimation) =>
-                                    const Analytics(),
-                            transitionsBuilder: (context, animation,
-                                secondaryAnimation, child) {
-                              const begin = Offset(1.0, 0.0);
-                              const end = Offset.zero;
-                              const curve = Curves.easeInOut;
-                              var tween = Tween(begin: begin, end: end)
-                                  .chain(CurveTween(curve: curve));
+        bottomNavigationBar: BottomAppBar(
+            shape: const CircularNotchedRectangle(),
+            color: Colors.deepPurple,
+            child: Container(
+              child: Row(
+                // mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: IconButton(
+                        icon: Icon(
+                          size: 30,
+                          Icons.analytics_outlined,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            PageRouteBuilder(
+                              pageBuilder:
+                                  (context, animation, secondaryAnimation) =>
+                                      const Analytics(),
+                              transitionsBuilder: (context, animation,
+                                  secondaryAnimation, child) {
+                                const begin = Offset(1.0, 0.0);
+                                const end = Offset.zero;
+                                const curve = Curves.easeInOut;
+                                var tween = Tween(begin: begin, end: end)
+                                    .chain(CurveTween(curve: curve));
 
-                              var offsetAnimation = animation.drive(tween);
+                                var offsetAnimation = animation.drive(tween);
 
-                              return SlideTransition(
-                                position: offsetAnimation,
-                                child: child,
-                              );
-                            },
-                          ),
-                        );
-                      }),
-                ),
-              ],
-            ),
-          )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const HomeWidget(),
-            ),
-          );
-        },
-        tooltip: 'Scan QR',
-        child: const Icon(Icons.camera_alt),
+                                return SlideTransition(
+                                  position: offsetAnimation,
+                                  child: child,
+                                );
+                              },
+                            ),
+                          );
+                        }),
+                  ),
+                ],
+              ),
+            )),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomeWidget(),
+              ),
+            );
+          },
+          tooltip: 'Scan QR',
+          child: const Icon(Icons.camera_alt),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
   }
 }
